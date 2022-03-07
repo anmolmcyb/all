3cd55e4be4a6f2ab2da82f38d72cf556df919944
anmolmcyb <anmolm@cybage.com>
2022-03-07 19:46:25 +0530

    update

---
+++ 03-09-21/js/1.4/js/script.js
@@ -0,0 +1,5 @@
+var msg = '<p><code>the script is located in ./js/script.js file</p></code>'
+function addNumbers(headParam, bodyParam) {
+    document.writeln(msg)
+    document.writeln('the sum of ', headParam, ' and ', bodyParam, ' is ', headParam+bodyParam, '.')
+}
